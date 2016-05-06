d8f12afdd667d3ae98802b15326e5037766c532d
nkielsky <bentzi.kielsky@gmail.com>
2016-05-05 21:30:57 -0700

    Changes to Python
    Changed the script to Python and added if else statements

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#Python Hello World
+
+print('Would you like to say hello world?')
+print('Press 1 for yes or 2 for no')
+answer = int(raw_input('  '))
+if answer == 1:
+    print('Hello Cruel world!!!!')
+elif answer == 2:
+    print('Well why did you start this is the first place??')
+else:
+    print('Goodbye!!!')
+print('Get some exercise outside.')
+
+  
+
